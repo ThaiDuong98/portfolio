267ec4da9335d2368462261319629c40fa7ab605
DuongEBST <duong.pham@ebst.tech>
2023-03-05 01:25:52 +0700

    add UI project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useElementOnScreen from '../../hook/useElementOnScreen';
+import './project.css'
 
 const Projects = ({scrollRef, observeActiveComponent}) => {
+  const [containerRef, isVisible] = useElementOnScreen({
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (isVisible) {
+      observeActiveComponent(5);
+    }
+  }, [isVisible]);
   return (
-    <div ref={scrollRef} id="project">Projects</div>
-  )
+    <section ref={scrollRef} id="project" className="project-container">
+      <div className="project-header" ref={containerRef}>
+        <a className={isVisible ? "active" : ""}>Projects</a>
+      </div>
+      <div className="project-layout">
+        <div className="project-card">
+          <div className="project-inner">
+            <div className="project-img">
+              <img src="/imgs/shop-store.png" />
+            </div>
+            <div className="project-detail">
+              <div className="content">
+                <p>Home store landing page</p>
+                <p>Status: building...</p>
+                <p>
+                  Website:{" "}
+                  <a href="https://shop-store-landing-page.netlify.app/" style={{color: "#fff"}}>
+                    visit website
+                  </a>
+                </p>
+                <div style={{display: "flex", flexWrap: "wrap"}}>
+                  <div className='html5'>HTML5</div> 
+                  <div className='css3'>CSS3</div>
+                  <div className='javascript'>Javascript</div> 
+                  <div className='responsive'>Responesive</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects
